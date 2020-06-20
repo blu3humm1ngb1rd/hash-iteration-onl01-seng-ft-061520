68e3d1d4a1e0a79c6d21ed8041b292e5314ab17e
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-20 18:30:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,10 +13,8 @@ end
 
 
 def age_appropriate_birthday(birthday_kids)
-under_12 = {}
- birthday_kids.select do under_12 = |name, age| age < 12
-end 
-under_12.do |name, age| puts "Happy Birthday #{name}! You are now #{age} years old!" 
+ birthday_kids.select do |name, age| age < 12
+ puts "Happy Birthday #{name}! You are now #{age} years old!" 
 
   
   end
